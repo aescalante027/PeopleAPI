fb6ad3a57e9225b51ed0364c308cb1e0764a7788
Alex Escalante <alexnescalante@unicorn.local>
2022-12-05 22:30:42 -0800

    Transition from Flask API to FastAPI. WIP

--- main.py
+++ main.py
@@ -1,59 +1,53 @@
-# This is a sample Python script.
-from flask import Flask, jsonify, request, abort
+from fastapi import FastAPI, HTTPException, Request, Body
 from DataModels.Person import Person
 from Controllers.PersonController import PersonController
+import uvicorn
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-app = Flask(__name__)
+app = FastAPI()
 personController = PersonController("settings.csv")
 
 
-@app.route('/person/all', methods=['GET'])
-def printAllUsers():
+@app.get('/person/all')
+def print_all_users():
     data = personController.getPeopleData()
     memory = {"data": data}
-    return jsonify(memory)
+    return memory
 
 
-@app.route('/person/<int:id>', methods=['GET'])
-def getPersonById(id):
+@app.get('/person/{id}')
+async def get_person_by_id(id):
     result = personController.getPersonById(id)
-    if result == None:
-        abort(400)
-    return jsonify(result)
+    if result is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return result
 
 
-@app.route('/person/add', methods=['POST'])
-def addNewPerson():
-    data = request.json
+@app.post('/person/add')
+async def add_new_person():
+    data: dict = Body(...)
+    print(data.values())
     id = None
     newPerson = Person(id, data["lastname"], data["firstname"], data["age"])
     personController.addNewPerson(newPerson)
-    return jsonify(personController.getPeopleData())
+    return personController.getPeopleData()
 
 
-@app.route('/person/delete/<int:id>', methods=['DELETE'])
-def removePerson(id):
+@app.delete('/person/delete/{id}')
+async def remove_person(id):
     personController.removePerson(id)
-    return jsonify(personController.getPeopleData())
+    return personController.getPeopleData()
 
 
-@app.route('/person/<int:id>/age/<int:age>', methods=['PUT'])
-def changeAge(id, age):
+@app.put('/person/{id}/age/{age}')
+async def change_age(id, age):
     personController.updateAge(id, age)
-    return jsonify(personController.getPersonById(id))
+    return personController.getPersonById(id)
 
 
-@app.route('/person/<int:id>/lastname/<lastname>', methods=['PUT'])
-def changeLastName(id, lastname):
+@app.put('/person/{id}/lastname/{lastname}')
+async def change_last_name(id, lastname):
     personController.updatePersonLastName(id, lastname)
-    return jsonify(personController.getPersonById(id))
+    return personController.getPersonById(id)
 
-
-if __name__ == '__main__':
-    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-    app.run(debug=True)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5050)
