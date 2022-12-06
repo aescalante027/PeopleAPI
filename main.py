1b55fda0155a038194390692f558b25aee5786ea
Alex Escalante <alexnescalante@unicorn.local>
2022-12-05 23:09:25 -0800

    Correction on port used

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ async def change_last_name(id, lastname):
     return personController.getPersonById(id)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5050)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
